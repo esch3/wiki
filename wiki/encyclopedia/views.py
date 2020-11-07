7e9ed73ea6aad1c8cd2dcea2810720be2245d3ac
r4don <compuradon@gmail.com>
2020-11-07 11:51:11 -0500

    added create new entry form and style for form

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,6 +7,10 @@ from . import util
 
 class SearchTitleForm(forms.Form):
     title = forms.CharField(label="Search")
+
+class CreateNewForm(forms.Form):
+    title = forms.CharField()
+    content = forms.CharField(widget=forms.Textarea)
     
 def match_title(title):
     # Returns True if a complete match
@@ -57,4 +61,9 @@ def search(request):
         })
 
 def new(request):
-    pass
+    new_entry= CreateNewForm()
+    return render(request, "encyclopedia/index.html", {
+        "title": "Encyclopedia",
+        "form": SearchTitleForm(),
+        "new_entry_form": new_entry
+    })
